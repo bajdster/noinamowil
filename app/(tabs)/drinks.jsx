4d36e005e1ce4477246bf64f896890e2073cd153
Bajda <bajdster@gmail.com>
2024-07-16 23:59:13 +0100

    implemented correct images loading

--- app/(tabs)/drinks.jsx
+++ app/(tabs)/drinks.jsx
@@ -1,18 +1,33 @@
-import { ScrollView, Text, StyleSheet } from 'react-native'
+import { ScrollView, Text, StyleSheet, Image } from 'react-native'
 import React, { useEffect, useState } from 'react'
 import { getRecipes } from '../../lib/recipeActions'
+import supabase from '../../lib/supabaseClient'
 
 const Drinks = () => {
 
 const [drinks, setDrinks] = useState()
 
+
+// useEffect(()=>
+// {
+//   async function getImages()
+//   {
+//     const { data, error } = await supabase.storage
+//   .from('recipe-images')
+//   .download('*')
+//   return data
+//   }
+
+//   console.log(getImages())
+// }, [])
+
 useEffect(()=>
 {
-  const getDrinks = async function()
+  const getDrinks = async () =>
   {
     const drinks = await getRecipes()
-    console.log(drinks.data[0].additional_info)
-    setDrinks(drinks.data)
+    console.log(drinks.data[0].image_url)
+    setDrinks(drinks)
     return drinks
   }
 
@@ -27,6 +42,10 @@ useEffect(()=>
     <ScrollView style={styles.drinksContainer}>
       <Text>Input wyszukiwarki</Text>
       <Text>Lista drinków</Text>
+      <Image 
+      source={{ uri: drinks.data[0].image_url }} 
+      style={{ width: 200, height: 200 }}
+    />
     </ScrollView>
   )
 }
